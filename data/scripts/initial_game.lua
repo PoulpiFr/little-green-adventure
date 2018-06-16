a8c5786cebcbebb2a1ce51616b20a17631ef58ad
Poulpi <contact@poulpi.fr>
2018-06-16 23:46:14 +0200

    Add shop items + first dialogs

--- data/scripts/initial_game.lua
+++ data/scripts/initial_game.lua
@@ -14,7 +14,7 @@ function initial_game:initialize_new_savegame(game)
 
   -- You can modify this function to set the initial life and equipment
   -- and the starting location.
-  game:set_starting_location("lga/village_main", "destination")
+  game:set_starting_location("lga/inside_shop", "inside_shop_destination")
 
   game:set_max_life(6)
   game:set_life(game:get_max_life())
